8e454c946a7cfbec70a4c0b518684b762b2c7ba4
Michael Bade <mbade314@gmail.com>
2020-05-13 16:18:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,17 +4,17 @@
 def key_for_min_value(name_hash)
   #set lowest value
   lowest_value = 0
-  lowest_key = nil
   #set lowest key
+  lowest_key = nil
+  #iterate over the name_hash argument with key and value as vars
   name_hash.each do |key, value|
+    #if the lowest value is 0 || or if value is less than lowest_value
     if lowest_value == 0 || value < lowest_value
+      #then the lowest_value becomes iterated value and lowest_key becomes iterated key
       lowest_value = value
       lowest_key = key
     end
   end
+  #return the key, or the item that is smallest
   lowest_key
-  #if the lowest value is 0 || or if value is less than lowest_value
-    #then the lowest_value becomes iterated value and lowest_key becomes iterated key
-    #end twice
-    #return value
 end
